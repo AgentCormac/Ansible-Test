012c14cb914909f80649400cfc304223a2007c90
AgentCormac <mark.higginbottom@gmail.com>
2016-10-03 10:52:58 +0100

    Start ssh-agent

--- scripts/ssh-keys.sh
+++ scripts/ssh-keys.sh
@@ -3,3 +3,5 @@ sudo cp -rf /home/vagrant/shared/ssh-keys/.ssh /home/vagrant
 sudo chown -R vagrant:vagrant /home/vagrant/.ssh
 sudo chmod 700 /home/vagrant/.ssh
 sudo chmod 600 /home/vagrant/.ssh/id_rsa
+eval $(ssh-agent)
+ssh-add /home/vagrant/.ssh/id_rsa
