7426bf278580f6393829dd0348328a788f910c22
AgentCormac <mark.higginbottom@gmail.com>
2016-10-05 13:32:54 +0100

    Authentication corrections

--- scripts/ssh-keys.sh
+++ scripts/ssh-keys.sh
@@ -3,5 +3,10 @@ sudo cp -rf /home/vagrant/shared/ssh-keys/.ssh /home/vagrant
 sudo chown -R vagrant:vagrant /home/vagrant/.ssh
 sudo chmod 700 /home/vagrant/.ssh
 sudo chmod 600 /home/vagrant/.ssh/id_rsa
-eval $(ssh-agent)
-ssh-add /home/vagrant/.ssh/id_rsa
+echo .
+echo These are the ssh keys to access this VM.
+echo There should be the vagrant public key and the control public key.
+cat /home/vagrant/.ssh/authorized_keys
+#No longer needed
+#eval $(ssh-agent)
+#ssh-add /home/vagrant/.ssh/id_rsa
